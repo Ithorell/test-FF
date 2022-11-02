ba4681ce3e39693b1277fb4608c49f4c6c399961
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-11-02 17:18:42 -0600

    added routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,23 +2,23 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//       order: [['name', 'ASC']],
-//     });
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
 
-//     const users = userData.map((project) => project.get({ plain: true }));
+    const users = userData.map((project) => project.get({ plain: true }));
 
-//     res.render('login', {
-//       users,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('login', {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/register', (req, res) => {
   if (req.session.logged_in) {
@@ -53,6 +53,5 @@ router.get('/teamSelect', (req, res) => {
 
   res.render('teamSelect');
 });
-
 
 module.exports = router;
